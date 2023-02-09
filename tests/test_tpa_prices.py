d82a144931af226235e0a0103fd4aaabb55d8635
Alexander Nemshilov <nemshilova@togizu.com>
2023-02-09 20:05:16 +0200

    added 3

--- tests/test_tpa_prices.py
+++ tests/test_tpa_prices.py
@@ -8,5 +8,8 @@ import pytest
 def test_find_prices(abuse_page):
     abuse_page.login()
     abuse_page.count_find_elements()
+    abuse_page.count_find_elements2()
+    abuse_page.count_find_elements3()
+    abuse_page.compare_dicts()
 
 
